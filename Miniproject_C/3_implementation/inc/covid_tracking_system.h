208bc4e099db6005c551a3ae9cdb6d59950a05c3
Abiramikoperundevi <101585225+Abiramikoperundevi@users.noreply.github.com>
2022-03-21 17:04:59 +0530

    Create covid_tracking_system.h

---
+++ Miniproject_C/3_implementation/inc/covid_tracking_system.h
@@ -0,0 +1,55 @@
+/**
+ * @file covid_tracking_system.h
+ * @author Abirami G ()
+ * @brief Covid and vaccinated status as like a tracking system
+ * @version 0.1
+ * @date 2022-03-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __COVID_TRACKING_SYSTEM_H__
+#define __COVID_TRACKING_SYSTEM_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* Displays the covid details with the information of Total case, Active case, Recovered case, Death case
+* @param[in] total_case 
+* @param[in] active_case
+* @param[in] recovered_case
+* @param[in] death_case
+* @return Result of the count of Total case, Active case, Recovered case, Death case
+*/
+void covid_details();
+
+/**
+* Displays the vaccinated details with the information of first dosage and second dosage
+* @param[in] first_dose 
+* @param[in] second_dose 
+* @return Result of the count of first dose and second dose
+*/
+void vaccinated_details()
+
+/**
+* Displays the status of the covid cases with the information of Total case, Active case, Recovered case, Death case
+* @param[in] total_case 
+* @param[in] active_case
+* @param[in] recovered_case
+* @param[in] death_case
+* @return Result of the count status of Total case, Active case, Recovered case, Death case
+*/
+void covid_status()
+
+
+/**
+* Displays the status of the vaccinated cases with the information of first dosage and second dosage
+* @param[in] first_dose 
+* @param[in] second_dose 
+* @return Result of the count status of first dose and second dose
+*/
+void vaccinated_status()
+
+#endif  /* #define __COVID_TRACKING_SYSTEM_H__ */
