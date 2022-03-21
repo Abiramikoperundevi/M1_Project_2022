68ecefada5276750aacdefcffdcdecd805030bcd
Abiramikoperundevi <101585225+Abiramikoperundevi@users.noreply.github.com>
2022-03-21 23:09:31 +0530

    Update covid_tracking_system.c

--- Miniproject_C/3_implementation/src/covid_tracking_system.c
+++ Miniproject_C/3_implementation/src/covid_tracking_system.c
@@ -6,16 +6,18 @@ long long int covid_details(long long int temp_total_case,long long int temp_act
 		s[temp_state].total_case=s[temp_state].total_case + temp_total_case;
 		s[temp_state].active_case=s[temp_state].active_case + temp_active_case;
 		s[temp_state].recovered_case=s[temp_state].recovered_case + temp_recovered_case;
-    		s[temp_state].death_case=s[temp_state].death_case + temp_death_case;
+        s[temp_state].death_case=s[temp_state].death_case + temp_death_case;
 }
 long long int vaccinated_details(long long int temp_first_dose,long long int temp_second_dose){
    
-        	v[temp_state].first_dose=v[temp_state].first_dose+temp_first_dose;
-    		v[temp_state].second_dose=v[temp_state].second_dose+temp_second_dose;
+        v[temp_state].first_dose=v[temp_state].first_dose+temp_first_dose;
+        v[temp_state].second_dose=v[temp_state].second_dose+temp_second_dose;
 }
 
-int covid_status(int temp_state)
+int covid_status(temp_state)
 {
+    if(temp_state<=4){
+        if(s[temp_state].total_case!=0){
         printf("\n*********** COVID STATUS **************");
         printf("\n|Total Cases              | %lld  |",s[temp_state].total_case);
         printf("\n-------------------------------");
@@ -27,13 +29,32 @@ int covid_status(int temp_state)
         printf("\n-------------------------------");
         
         printf("\n**************************************");
+        }
+        else
+        {
+            printf("\n------The details of the covid cases yet not be filled------\n");
+        }
+    }
+    else{
+        printf("\nEnter the valid choice");
+    }
 }
-int vaccinated_status(int temp_state){
+int vaccinated_status(temp_state){
+    if(temp_state<=4){
+        if(v[temp_state].first_dose!=0 && v[temp_state].second_dose!=0){
         printf("\n*********** VACCINATED STATUS **************");
         printf("\n|1st dosage total count           | %lld |",v[temp_state].first_dose);
         printf("\n-------------------------------");
         printf("\n|2nd dosage total count           | %lld |",v[temp_state].second_dose);
         printf("\n-------------------------------");
     
-        printf("\n**************************************");
+        printf("\n**********************************************");
+        }
+        else
+        printf("\n------The details of the vaccinated cases yet not be filled------\n");
+    }
+    else
+    {
+        printf("\nEnter the valid choice");
+    }
 }
