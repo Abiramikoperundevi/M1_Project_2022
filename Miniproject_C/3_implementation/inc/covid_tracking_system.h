6df810e3b2f5c078674c15931742d31b3305a63b
Abiramikoperundevi <101585225+Abiramikoperundevi@users.noreply.github.com>
2022-03-21 20:13:17 +0530

    Update covid_tracking_system.h

--- Miniproject_C/3_implementation/inc/covid_tracking_system.h
+++ Miniproject_C/3_implementation/inc/covid_tracking_system.h
@@ -23,7 +23,7 @@
 * @param[in] death_case
 * @return Result of the count of Total case, Active case, Recovered case, Death case
 */
-void covid_details()
+long long int covid_details(long long int temp_total_case,long long int temp_active_case,long long int temp_recovered_case,long long int temp_death_case)
 
 /**
 * Displays the vaccinated details with the information of first dosage and second dosage
@@ -31,25 +31,21 @@ void covid_details()
 * @param[in] second_dose 
 * @return Result of the count of first dose and second dose
 */
-void vaccinated_details()
+long long int vaccinated_details(long long int temp_first_dose,long long int temp_second_dose)
 
 /**
 * Displays the status of the covid cases with the information of Total case, Active case, Recovered case, Death case
-* @param[in] total_case 
-* @param[in] active_case
-* @param[in] recovered_case
-* @param[in] death_case
+* @param[in] temp_state 
 * @return Result of the count status of Total case, Active case, Recovered case, Death case
 */
-void covid_status()
+int covid_status(int temp_state)
 
 
 /**
 * Displays the status of the vaccinated cases with the information of first dosage and second dosage
-* @param[in] first_dose 
-* @param[in] second_dose 
+* @param[in] temp_state
 * @return Result of the count status of first dose and second dose
 */
-void vaccinated_status()
+int vaccinated_status(temp_state)
 
 #endif  /* #define __COVID_TRACKING_SYSTEM_H__ */
