1890ddda52070ed8e9c2eedad579956dc3adce33
Abiramikoperundevi <101585225+Abiramikoperundevi@users.noreply.github.com>
2022-03-26 01:03:46 +0530

    Update covid_tracking_system.h

--- Miniproject_C/3_implementation/inc/covid_tracking_system.h
+++ Miniproject_C/3_implementation/inc/covid_tracking_system.h
@@ -39,7 +39,7 @@ struct vaccinated_detail
 * @return Result of the count of Total case, Active case, Recovered case, Death case
 */
 
-long int covid_details(struct covid_detail s[], int temp_state, long int temp_total_case,long int temp_active_case,long int temp_recovered_case,long int temp_death_case)
+void covid_details(struct covid_detail s[], int temp_state, long int temp_total_case,long int temp_active_case,long int temp_recovered_case,long int temp_death_case)
 
 /**
 * Displays the vaccinated details with the information of first dosage and second dosage
@@ -48,7 +48,7 @@ long int covid_details(struct covid_detail s[], int temp_state, long int temp_to
 * @return Result of the count of first dose and second dose
 */
 
-long int vaccinated_details(struct vaccinated_detail v[], int temp_state, long int temp_first_dose, long int temp_second_dose)
+void vaccinated_details(struct vaccinated_detail v[], int temp_state, long int temp_first_dose, long int temp_second_dose)
 
 /**
 * Displays the status of the covid cases with the information of Total case, Active case, Recovered case, Death case
@@ -56,13 +56,13 @@ long int vaccinated_details(struct vaccinated_detail v[], int temp_state, long i
 * @return Result of the count status of Total case, Active case, Recovered case, Death case
 */
 
-int covid_status(int temp_state)
+void covid_status(int temp_state)
 /**
 * Displays the status of the vaccinated cases with the information of first dosage and second dosage
 * @param[in] temp_state
 * @return Result of the count status of first dose and second dose
 */
 
-int vaccinated_status(int temp_state)
+void vaccinated_status(int temp_state)
 
 #endif  /* #define __COVID_TRACKING_SYSTEM_H__*/
